786bdc7bb952246834b82eaa968ab3faa5dd37d3
isa <54730392+isabellozano@users.noreply.github.com>
2020-03-16 21:39:23 -0700

    added app and mainRoute.js for starter code

--- app/app.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MainRouter from './components/MainRouter';
-
-ReactDOM.render(<MainRouter />, document.getElementById("app"));
---
+++ client/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import mainRoute from './components/mainRoute';
+
+ReactDOM.render(<mainRoute />, document.getElementById("app"));
